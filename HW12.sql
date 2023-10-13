12b59450496c47e245386d6c60d2d67fb5a3070f
Burcu Tokol <56488562+burcutokol@users.noreply.github.com>
2023-10-14 00:19:31 +0300

    Add files via upload

---
+++ HW12.sql
@@ -0,0 +1,21 @@
+SELECT COUNT(*) AS "Average"
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+--
+SELECT COUNT(*) AS "MaxRate"
+FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+--
+
+SELECT * FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+--
+
+SELECT customer_id, COUNT(*) AS "Count"
+FROM payment
+GROUP BY customer_id
+ORDER BY "Count" DESC
+LIMIT 10; 
+
+
